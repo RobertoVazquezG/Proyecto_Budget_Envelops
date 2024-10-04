0711ab27b8f1075fb67ddd9fa0d6890aac482d01
Roberto <roberiv_vaga@hotmail.com>
2024-10-03 21:56:28 -0600

    Se agregó método POST con la funcionalidad de transferir budgets de un envelop a otro. Se modificó el método POST que retira fondos, se agregó que no puede sacar mayor cantidad que el monto existente.

--- helpers/budget_envelope.js
+++ helpers/budget_envelope.js
@@ -34,14 +34,14 @@ routerEnvelop.get('/:id', (req, res, next) => {
 });
 
 routerEnvelop.put('/:id/:withdraw', (req, res, next) => {
-    const budgetId = req.params.id;
+    const budgetId = parseInt(req.params.id);
     const withdrawal = parseInt(req.params.withdraw);
-    const found = budget_Array.find(element => element.id === parseInt(budgetId));
-    if (found && found.Categorie && typeof found.Limit === 'number') {
+    const found = budget_Array.find(element => element.id === budgetId);
+    if (found && typeof withdrawal === 'number' && withdrawal < found.Limit) {
         found.Limit = found.Limit - withdrawal;
         res.send(found);
     } else {
-        res.status(400).send("The request cannot be proccesed.");
+        res.status(400).send(`The request cannot be proccesed or don't have sufficient founds.`);
     }
 });
 
@@ -54,6 +54,20 @@ routerEnvelop.delete('/:id', (req, res, next) => {
         res.status(200).send("Item deleted correctly.");
     } else {
         res.status(404).send("The item does not exists. Try with a diferent id");
+    }
+});
+
+routerEnvelop.post('/transfer/:from/:to', (req, res, next) => {
+    const budgetFrom = parseInt(req.params.from);
+    const budgetTo = parseInt(req.params.to);
+    const foundFrom = budget_Array.find(element => element.id === budgetFrom);
+    const foundTo = budget_Array.find(element => element.id === budgetTo);
+    if(foundFrom && foundTo && foundFrom.Limit > 0 ) {
+        foundTo.Limit = foundTo.Limit + foundFrom.Limit;
+        foundFrom.Limit = 0;
+        res.status(200).send(foundTo);
+    } else {
+        res.status(400).send("You cannot perform this action or have insufficient founds.");
     }
 });
 
