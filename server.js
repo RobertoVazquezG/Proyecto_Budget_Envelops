e622717f6df0306eb6b53c7f05071e64019e3b73
Roberto <roberiv_vaga@hotmail.com>
2024-10-04 00:56:32 -0600

    Se agrega la libreria CORS, para poder compartir recursos con el front-end del proyecto

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const routerEnvelop = require('./helpers/budget_envelope.js');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
