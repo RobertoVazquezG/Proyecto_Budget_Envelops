d8ba4d62303836ff0683b518953d04701ca9cdad
Roberto <roberiv_vaga@hotmail.com>
2024-10-04 17:34:02 -0600

    Se agregó la funcionalidad para el botón para solicitar las budget envelop creadas.

--- front-end/script.js
+++ front-end/script.js
@@ -3,6 +3,9 @@ let categoryInfo = document.getElementById('Category');
 let limitInfo = document.getElementById('Limit');
 let testOk = document.getElementById('testOk');
 let test = document.getElementById('test');
+let budgetCreated =document.getElementById('budgetCreated');
+let getBudgets = document.getElementById('getBudgets');
+let budgetRequested = document.getElementById('budgetRequested');
 
 
 const testConection = async () => {
@@ -36,14 +39,32 @@ const createBudget = async () => {
         if(response.ok) {
             const jsonResponse = await response.json();
             console.log(jsonResponse);
+            budgetCreated.value=JSON.stringify(jsonResponse, null, 2);
         } else {
             throw new Error('Request Failed!');
         }
     } catch(error) {
         console.log(error);
+        budgetCreated.value=error;
     }
 }
 
+const requestBudgets = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/envelops');
+        console.log(response);
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            budgetRequested.value = JSON.stringify(jsonResponse, null, 2);
+        } else { 
+        throw new Error('Request failed. No budgets envelops created.');
+        }
+    } catch(error){
+        console.log(error);
+        budgetRequested.value=error;
+    }
+}
 
 test.addEventListener('click', testConection);
 sendButton.addEventListener('click', createBudget);
+getBudgets.addEventListener('click', requestBudgets);
