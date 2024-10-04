c2440ebe1a13cf0979c1e24d91d3cc80c5153f39
Roberto <roberiv_vaga@hotmail.com>
2024-10-04 16:16:40 -0600

    Se agregó la funcionalidad del método POST, para crear el budget envelop, totalmente funcional.

--- front-end/script.js
+++ front-end/script.js
@@ -1,17 +1,17 @@
 let sendButton = document.getElementById('sendButton');
 let categoryInfo = document.getElementById('Category');
 let limitInfo = document.getElementById('Limit');
-let parrafo = document.getElementById('parr');
+let testOk = document.getElementById('testOk');
+let test = document.getElementById('test');
 
 
-
-const createBudget = async () => {
+const testConection = async () => {
     try {
         const response = await fetch('http://localhost:3000');
         console.log(response);
         if(response.ok) {
             const textResponse = await response.text();
-            parrafo.innerHTML = textResponse;
+            testOk.innerHTML = textResponse;
         } else { 
         throw new Error('Request failed.');
         }
@@ -20,5 +20,30 @@ const createBudget = async () => {
     }
 }
 
+const createBudget = async () => {
+    const category = categoryInfo.value;
+    const limit = parseInt(limitInfo.value);
+    try {
+        const response = await fetch('http://localhost:3000/envelops', 
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                  },
+                body: JSON.stringify({Category: category, Limit: limit})
+            }
+        )
+        if(response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+        } else {
+            throw new Error('Request Failed!');
+        }
+    } catch(error) {
+        console.log(error);
+    }
+}
 
+
+test.addEventListener('click', testConection);
 sendButton.addEventListener('click', createBudget);
